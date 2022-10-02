551469cd44163fea5c16433fb273043e1ef7442f
Svetlana <svgf@mail.ru>
2022-10-02 22:09:26 +0300

    Задача 47 работает

---
+++ Exmpl050/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// [1, 7] -> такого числа в массиве нет
+
+Console.Clear();
+
+int [,] array = { {1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4} };
+
+void OutPutArray (int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"   " + arr[i,j]);
+        }
+        Console.WriteLine();
+    }
+}
+OutPutArray (array);
+
+int countRow = array.GetLength(0);
+int countColumn = array.GetLength(1);
+
+Console.WriteLine("Введите позицию элемента массива:");
+Console.Write("Строка: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Столбец: ");
+int column = int.Parse(Console.ReadLine());
+
+if (row > (countRow-1) | column > countColumn-1) 
+{
+    Console.WriteLine($"В массиве не существует элемента для указанной позиции - [" + row + ", " + column + "]");
+}
+else
+{
+    Console.WriteLine($"Элемент массива = " + array[row,column]);
+}
---
+++ Exmpl052/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int [,] array = { {1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4} };
+int countRow = array.GetLength(0);
+
+int countColumn = array.GetLength(1);
+
+double SummColumn (int [,] arr, ref int j)
+{
+double resultSum = 0;
+for (int i=0; i < countRow; i++)
+{
+	resultSum = resultSum + arr [i,j];
+}
+    return resultSum / countRow;
+}
+
+double average = 0;
+for (int j=0; j < countColumn; j++)
+{
+    average = SummColumn (array, ref j);
+    Console.WriteLine("Среднее арифметическое столбца {1} = {0:f2}", average, j);
+}
+
